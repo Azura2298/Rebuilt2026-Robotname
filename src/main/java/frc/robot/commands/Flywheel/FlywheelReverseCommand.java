d789432b2e3bfcb8792cc620333f34ea09e30681
Azura2298 <james-mcewan@outlook.com>
2026-01-14 18:59:11 -0500

    commands for flywheel

--- src/main/java/frc/robot/commands/Flywheel/FlywheelReverseCommand.java
+++ src/main/java/frc/robot/commands/Flywheel/FlywheelReverseCommand.java
@@ -1,4 +1,4 @@
-x// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -7,17 +7,16 @@ package frc.robot.commands.Flywheel;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class FlywheelForwardCommand extends Command {
+public class FlywheelReverseCommand extends Command {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final FlywheelSubsystem flywheel_subsystem;
 
-
-  /** Creates a new FlywheelForwardCommand. */
-    public FlywheelForwardCommand() {
-      flywheel_subsystem = subsystem;
-      // Use addRequirements() here to declare subsystem dependencies.
-      addRequirements(subsystem);
-    }
+  /** Creates a new FlywheelReverseCommand. */
+  public FlywheelReverseCommand(FlywheelSubsystem subsystem) {
+    flywheel_subsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
 
   // Called when the command is initially scheduled.
   @Override
--- src/main/java/frc/robot/commands/Flywheel/FlywheelStopCommand.java
+++ src/main/java/frc/robot/commands/Flywheel/FlywheelStopCommand.java
@@ -1,4 +1,4 @@
-x// Copyright (c) FIRST and other WPILib contributors.
+// Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -7,13 +7,12 @@ package frc.robot.commands.Flywheel;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class FlywheelForwardCommand extends Command {
+public class FlywheelStopCommand extends Command {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final FlywheelSubsystem flywheel_subsystem;
 
-
-  /** Creates a new FlywheelForwardCommand. */
-    public FlywheelForwardCommand() {
+    /** Creates a new FlywheelStop. */
+    public FlywheelStopCommand(FlywheelSubsystem subsystem) {
       flywheel_subsystem = subsystem;
       // Use addRequirements() here to declare subsystem dependencies.
       addRequirements(subsystem);
