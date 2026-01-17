aa8defbb2f4f39ad124d04fcb0a06c446cfb4032
Azura2298 <james-mcewan@outlook.com>
2026-01-16 19:02:29 -0500

    Create BuildConstants.java

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Rebuilt2026-Robotname";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 16;
+  public static final String GIT_SHA = "2c1b2f07bc1abb0b91f8c4da0455bfc0b8a74088";
+  public static final String GIT_DATE = "2026-01-14 20:00:27 EST";
+  public static final String GIT_BRANCH = "flywheel";
+  public static final String BUILD_DATE = "2026-01-16 18:51:04 EST";
+  public static final long BUILD_UNIX_TIME = 1768607464754L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
